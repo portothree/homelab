56ca6487cd89d57b70e18974b87c360384234559
Gustavo Porto <portothree@gmail.com>
2022-03-13 14:45:34 +0000

    chore: add lara hosts with nixos config

---
+++ hosts/lara/README.md
@@ -0,0 +1,9 @@
+# Lara
+
+## Overview
+
+Machine running as a VM inside Proxmox VE.
+
+## Specs
+
+## Installation
---
+++ hosts/lara/configuration.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [ ../common.nix ./hardware-configuration.nix ];
+  boot = {
+    loader = {
+	systemd-boot = {
+		enable = true;
+	};
+	efi = {
+		canTouchEfiVariables = true;
+	};
+    };
+  };
+  networking = {
+    hostName = "lara";
+    useDHCP = false;
+    interfaces = { ens18 = { useDHCP = true; }; };
+    extraHosts = ''
+      192.168.1.100 pve
+    '';
+    nameservers = [ "208.67.222.222" "208.67.220.220"];
+    firewall = {
+      allowedTCPPorts = [ 22 80 443 ];
+      allowedUDPPorts = [ ];
+    };
+  };
+  services = {
+    openssh = { enable = true; };
+  };
+  users = {
+    users = {
+      porto = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+      };
+    };
+  };
+  environment = { systemPackages = with pkgs; [ git curl k3s k9s kubectl fluxcd fluxctl ]; };
+  system = { stateVersion = "21.11"; };
+}
