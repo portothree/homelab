1de2993c1be7e2b306a03e450b8d0ddbc8db489a
Gustavo Porto <gus@p8s.co>
2023-05-19 17:08:35 +0100

    feat(jorel): v4l2loopback

--- hosts/jorel/configuration.nix
+++ hosts/jorel/configuration.nix
@@ -21,6 +21,11 @@
       systemd-boot = { enable = true; };
       efi = { canTouchEfiVariables = true; };
     };
+    extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback.out ];
+    kernelModules = [ "v4l2loopback" "snd-aloop" ];
+    extraModprobeConfig = ''
+      options v4l2loopback exclusive_caps=1 card_label="Virtual Camera"
+    '';
   };
   fileSystems."/home" = {
     device = "/dev/pool/home";
