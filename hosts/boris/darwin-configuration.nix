1c72f33bcf582a09ce9778c73d74666cd54b74c2
Gustavo Porto <gus@portothr.ee>
2023-12-10 19:09:25 +0000

    Revert "feat(hosts/boris): add steam cask"

--- hosts/boris/darwin-configuration.nix
+++ hosts/boris/darwin-configuration.nix
@@ -14,7 +14,7 @@
     enable = true;
     global = { lockfiles = true; };
     brews = [ "pyqt@6" "syncthing" ];
-    casks = [ "docker" "google-chrome" "anki" "steam" ];
+    casks = [ "docker" "google-chrome" "anki" ];
     masApps = { "tailscale" = 1475387142; };
   };
   fonts = {
